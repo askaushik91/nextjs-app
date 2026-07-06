162e7a395d0c783675df159d4ec5d613d341d972
askaushik91 <kaushik.ashish91@outlook.com>
2026-07-06 15:57:42 +0530

    chore: comment out TestimonialsSection on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
       <TeamSection />
       <ImageGallerySection />
       <CloudFarmProgram />
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
       <FAQSection />
       <MapSection />
     </>
